3465e68f1dcb737255a4acf59cfb7d352e1c6460
fazzyA <faz.pak@gmail.com>
2021-06-15 14:44:48 +0500

    .

--- backend/server.js
+++ backend/server.js
@@ -37,6 +37,9 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV != 'development') {
   app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.get('/test', (req, res) => {
+    res.send('API is running....')
+  })
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
